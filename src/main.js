cdbd31312fec8d5babf4293fb1d98370dadf6c58
Igor Krasylnikov <garikkrass@gmail.com>
2025-02-21 16:28:34 +0100

    swiper is set to work

--- src/main.js
+++ src/main.js
@@ -1,47 +1,92 @@
-import Swiper from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+// import Swiper from 'swiper';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+// import 'swiper/css/scrollbar';
+
+// const swiper = new Swiper('.swiper', {
+//   // containerModifierClass: 'swiper-container',
+//   wrapperClass: 'swiper-wrapper',
+//   slideClass: 'swiper-slide',
+//   direction: 'horizontal',
+//   centeredSlides: true,
+//   loop: false,
+//   freeMode: false,
+//   resistance: true,
+//   resistanceRatio: 0.85,
+
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
+
+// const swiperSlide = new Swiper('.mySwiper', {
+//   slidesPerView: 1,
+//   spaceBetween: 0,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 0,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 10,
+//     },
+//     1024: {
+//       slidesPerView: 3,
+//       spaceBetween: 30,
+//     },
+//   },
+// });
 
 const swiper = new Swiper('.swiper', {
+  loop: false,
+  centeredSlides: true,
   direction: 'horizontal',
-  loop: true,
-
+  lazyloading: true,
+  // effect: 'fade',
+  freeMode: false,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
   scrollbar: {
     el: '.swiper-scrollbar',
   },
-});
-
-let swiperSlide = new Swiper('.mySwiper', {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
   breakpoints: {
-    640: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768: {
       slidesPerView: 2,
       spaceBetween: 20,
     },
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
     1024: {
-      slidesPerView: 5,
-      spaceBetween: 50,
+      slidesPerView: 3,
+      spaceBetween: 40,
     },
   },
 });
